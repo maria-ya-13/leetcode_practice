e1fddb919fbcd968e9bd42eb8e2856224ad3c57a
orca <82097521+orca-lab@users.noreply.github.com>
2021-05-02 09:19:37 -0900

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,7 +1,6 @@
 # https://leetcode.com/problems/two-sum/
 
-# how to use hash-map here? 
-
+# O(n^2) solution
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:  
         for pos_num_1 in range(len(nums)):
@@ -10,4 +9,14 @@ class Solution:
                     continue
                 if (nums[pos_num_1]+nums[pos_num_2]) == target:
                     return [pos_num_1,pos_num_2]
-            
+ 
+# O(n) solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:  
+        mapped = {}
+        for position, number in enumerate(nums):
+            required = target - number
+            if required in mapped:
+                req_pos = mapped[required]
+                return [req_pos, position]
+            mapped[number] = position
